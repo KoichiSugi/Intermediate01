bbe8ff1fc744d7286bf605880dc62298d24bb684
KoichiSugi <43499213+KoichiSugi@users.noreply.github.com>
2021-05-23 22:28:33 +1000

    Add files via upload

---
+++ js/Datatables.js
@@ -0,0 +1,31 @@
+function viewData() {
+  $.ajax({
+    url: "../q2.tabledata.txt",
+    method: "GET",
+  }).done(function (data) {
+    $("#example")
+      .html(data)
+      .DataTable({
+        initComplete: function () {
+          this.api()
+            .columns([3]) //specify the colum number
+            .every(function () {
+              var column = this;
+              var select = $('<select><option value=""></option></select>')
+                .appendTo($(column.header()))
+                .on("change", function () {
+                  var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                  column.search(val ? "^" + val + "$" : "", true, false).draw();
+                });
+              column
+                .data()
+                .unique()
+                .sort()
+                .each(function (d, j) {
+                  select.append('<option value="' + d + '">' + d + "</option>");
+                });
+            });
+        },
+      });
+  });
+}
